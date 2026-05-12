7689ee5c92ef92901a3558411d3c7c828ac18974
Mustafa Alkan <alkanmustafax@gmail.com>
2026-05-12 16:52:55 +0200

    Order model created

---
+++ backend/models/Order.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+
+  restaurantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Restaurant",
+    required: true
+  },
+
+  items: [
+    {
+      foodId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Food",
+        required: true
+      },
+
+      quantity: {
+        type: Number,
+        default: 1,
+        min: 1
+      }
+    }
+  ],
+
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+
+  status: {
+    type: String,
+    enum: ["pending", "preparing", "on_the_way", "delivered", "cancelled"],
+    default: "pending"
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("Order", orderSchema);
