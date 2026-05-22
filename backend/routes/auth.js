0ad5366b4c1bd501366a0e45b29cdcec2a744495
Mustafa Alkan <alkanmustafax@gmail.com>
2026-05-22 17:40:01 +0200

    add user validation and password hashing

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,21 +1,38 @@
+const bcrypt = require('bcrypt');
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 
 router.post("/register", async (req,res) =>{
 
-    const {name, email, password} = req.body;
-  
+    const {name, email,password} = req.body;
 
     try{
+
+        if (!email || !password) {
+        return res.status(400).json({ message: "Missing credentials" });
+        }
+
+        if(!password || password.length <6){
+            return res.status(400).json({message: "Password is too short."});
+        }
+
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            return res.status(400).json({message: "Email already exists."});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const NewUser = new User({
             name,
             email,
-            password
+            password : hashedPassword
         });
 
         await NewUser.save();
-        res.json({message:"User succesfully saved."});
+        res.json({message:"User successfully saved."});
     }
     catch(err){
         res.status(500).json({message:err.message});
@@ -35,13 +52,15 @@ router.post("/login", async (req,res)=>{
             return res.status(400).json({message :"User not found"});
         }
 
-        if(user.password !== password){
-            return res.status(400).json({message:"Password is wrong."});
+        const isMatch = await bcrypt.compare(password,user.password);
+    
+        if(!isMatch){
+            return res.status(400).json({ message : "Wrong password"});
         }
 
-        res.json({message:"You succesfully logged in."})
-
+        res.json({message:"You successfully logged in."})
     }
+
     catch(err){
         res.status(500).json({message: err.message})
     }
