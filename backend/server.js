fdbe7a50b21d223574a9045f3f9717aa4aecc2c4
Mustafa Alkan <alkanmustafax@gmail.com>
2026-08-04 18:05:39 +0200

    Add restaurant routes to API

--- backend/server.js
+++ backend/server.js
@@ -11,21 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 const authRoutes = require("./routes/auth");
+const restaurantsRoutes = require("./routes/restaurant")
 
-app.use("/auth" ,authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/restaurants", restaurantsRoutes)
 
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("DB error:", err));
 
-// 🧪 test route
+// test route
 app.get("/", (req, res) => {
   res.send("API is running 🍕");
 
 });
 
-// 🚀 server başlatma
+// server start
 app.listen(process.env.PORT, () => {
   console.log("Server running on port " + process.env.PORT);
 });
