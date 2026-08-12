ba963041e5836a1c3dce782a321c3ef422df46a2
Mustafa Alkan <alkanmustafax@gmail.com>
2026-08-12 15:39:57 +0200

    fetch restaurant by id

--- backend/routes/restaurant.js
+++ backend/routes/restaurant.js
@@ -3,18 +3,34 @@ const restaurant = require("../models/Restaurant");
 
 const router = express.Router();
 
-router.get("/", async (req,res) =>{
-    try{
-        const restaurants = await restaurant.find();
+router.get("/", async (req, res) => {
+  try {
+    const restaurants = await restaurant.find();
 
-        res.json(restaurants);
+    res.json(restaurants);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const restaurantData = await restaurant.findById(req.params.id);
+
+    if (!restaurantData) {
+      return res.status(404).json({
+        message: "Restaurant not found"
+      });
     }
 
-    catch(err){
-        res.status(500).json({
-            message: err.message
-        });
-    }
+    res.json(restaurantData);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    });
+  }
 });
 
 module.exports = router;
